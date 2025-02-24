c5dc31dd5c94023bdb9362c8d1903ff5cf719d02
congquynguyen296 <congquynguyen296@gmail.com>
2025-02-24 09:13:16 +0700

    change routes

---
+++ src/routers/AppRouter.jsx
@@ -0,0 +1,31 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import HomePage from "../pages/HomePage";
+import StoryDetailPage from "../pages/StoryDetailPage";
+import LoginPage from "../pages/LoginPage";
+import RegisterPage from "../pages/RegisterPage";
+import ConfirmEmailPage from "../pages/ConfirmEmailPage";
+import StoryContent from "../features/story/StoryContent";
+
+export default function AppRouter() {
+  return (
+    <BrowserRouter>
+      {" "}
+      {/* Bao bọc ứng dụng với BrowserRouter */}
+      <Routes>
+        {" "}
+        {/* Điều hướng mặc định */}
+        <Route path="/" element={<Navigate to="/home-page" replace />} />
+        {/* Sử dụng Routes để định nghĩa các route */}
+        <Route path="/home-page" element={<HomePage />} />
+        <Route path="/story/:code" element={<StoryDetailPage />} />
+        <Route
+          path="/story/:storyCode/:chapterNumber"
+          element={<StoryContent />}
+        />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/register/confirm-otp" element={<ConfirmEmailPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
